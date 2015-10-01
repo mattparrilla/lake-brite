4a3c19f374a7f5815f4f715dffd6f9ff0d72797b
Matthew Parrilla <matthew.parrilla@gmail.com>
2015-10-01 07:55:16 -0400

    Make GIF from matrix

--- matrix_to_gif.py
+++ matrix_to_gif.py
@@ -1,16 +1,41 @@
 from PIL import Image
 from numpy import asarray
+from libs.images2gif import writeGif
 
-matrix = []
-for i in range(0, 10):
-    matrix.append([])
-    for j in range(0, 15):
-        matrix[i].append([])
-        for k in range(0, 50):
-            if i == j:
-                matrix[i][j].append((k * 5, 0, 0))
-            else:
-                matrix[i][j].append((0, 0, 0))
+SAVE_TO_DIR = 'gif'
 
-for i, frame in enumerate(matrix):
-    Image.fromarray(asarray(matrix[i], 'uint8')).show()
+
+def generate_sample_matrix(x=50, y=15, z=10):
+    """Build a 3D matrix of given dimensions"""
+    matrix = []
+    for i in range(0, 10):
+        matrix.append([])
+        for j in range(0, 15):
+            matrix[i].append([])
+            for k in range(0, 50):
+                if i == j:
+                    matrix[i][j].append((k * 5, 0, 0))
+                else:
+                    matrix[i][j].append((0, 0, 0))
+
+    return matrix
+
+
+def generate_pil_images(matrix):
+    """Generate an array of PIL images from a matrix"""
+    return [Image.fromarray(asarray(matrix[i], 'uint8'))
+        for i, f in enumerate(matrix)]
+
+
+def generate_gif(images, name, directory=SAVE_TO_DIR, duration=0.125):
+    """Creates a gif from a list of PIL images"""
+
+    gif_name = "%s/%s.gif" % (directory, name)
+    writeGif(gif_name, images, duration)
+    return gif_name
+
+matrix = generate_sample_matrix()
+images = generate_pil_images(matrix)
+gif = generate_gif(images, 'test')
+
+# next step: get RGB out of matrix (unless passing image, apply color map)
